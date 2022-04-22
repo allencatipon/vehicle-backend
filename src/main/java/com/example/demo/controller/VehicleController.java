4ddba2f16b9ebb2f8488378eb646776f757572d0
Allen Catipon <allen.catipon@softvision.com>
2022-04-22 15:58:09 +0800

    add logic for searchBy=all

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -3,10 +3,11 @@ package com.example.demo.controller;
 import com.example.demo.model.Vehicle;
 import com.example.demo.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 @CrossOrigin("http://localhost:3000/")
 @RestController
@@ -17,22 +18,25 @@ public class VehicleController {
 
     @GetMapping("/vehicle")
     public List<Vehicle> getVehicle(@RequestParam String searchBy,
-                                    @RequestParam String searchValue){
-        if(Objects.nonNull(searchValue)){
-            switch(searchBy) {
-                case "id":
-                    return this.vehicleRepository.findAllById(Long.valueOf(searchValue));
-                case "variant":
-                    return this.vehicleRepository.findAllByVariantIgnoreCase(searchValue);
-                case "brand":
-                    return this.vehicleRepository.findAllByBrandIgnoreCase(searchValue);
-                case "color":
-                    return this.vehicleRepository.findAllByColorIgnoreCase(
-                            searchValue);
-                case "engineCapacity":
-                    return this.vehicleRepository.findAllByEngineCapacity(Long.valueOf(searchValue));
+                                    @RequestParam String searchValue) {
+        if (!ObjectUtils.isEmpty(searchBy)) {
+            if (!ObjectUtils.isEmpty(searchValue)) {
+                switch (searchBy) {
+                    case "id":
+                        return this.vehicleRepository.findAllById(Long.valueOf(searchValue));
+                    case "variant":
+                        return this.vehicleRepository.findAllByVariantIgnoreCase(searchValue);
+                    case "brand":
+                        return this.vehicleRepository.findAllByBrandIgnoreCase(searchValue);
+                    case "color":
+                        return this.vehicleRepository.findAllByColorIgnoreCase(
+                                searchValue);
+                    case "engineCapacity":
+                        return this.vehicleRepository.findAllByEngineCapacity(Long.valueOf(searchValue));
+                }
             }
+            return this.vehicleRepository.findAll();
         }
-        return this.vehicleRepository.findAll();
+        return Collections.emptyList();
     }
 }
