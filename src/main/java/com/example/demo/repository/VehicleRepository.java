fbd5f77c4610937a625b24508d2cfd1852b7f084
drozencarl23 <rozen.delacruz@ph.softvision.global>
2022-04-25 17:40:09 +0800

    Add Pagination

--- src/main/java/com/example/demo/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -1,17 +1,18 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Vehicle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
-    List<Vehicle> findAllById(Long id);
-    List<Vehicle> findAllByVariantIgnoreCase(String variant);
-    List<Vehicle> findAllByBrandIgnoreCase(String brand);
-    List<Vehicle> findAllByColorIgnoreCase(String color);
-    List<Vehicle> findAllByEngineCapacity(Long engineCapacity);
+public interface VehicleRepository extends PagingAndSortingRepository<Vehicle,  Integer> {
+    Page<Vehicle> findAllById(Long id, Pageable pageable);
+    Page<Vehicle> findAllByVariantIgnoreCase(String variant, Pageable pageable);
+    Page<Vehicle> findAllByBrandIgnoreCase(String brand, Pageable pageable);
+    Page<Vehicle> findAllByColorIgnoreCase(String color, Pageable pageable);
+    Page<Vehicle> findAllByEngineCapacity(Long engineCapacity, Pageable pageable);
 
 }
